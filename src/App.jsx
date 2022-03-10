ce1f29592fe1bc923021803ab33c940b0c6dc04f
Dan Forder <dan.forder@nology.io>
2022-03-10 17:21:54 +0000

    post challenge codealong

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.scss";
+import Button from "./components/Button/Button";
 import CardContainer from "./components/CardContainer/CardContainer";
-import RangeInput from "./components/RangeInput/RangeInput";
 
 const App = () => {
   const [users, setUsers] = useState([]);
-  const [numberOfUsers, setNumberOfUsers] = useState(5);
 
-  const url = "https://randomuser.me/api";
+  const url = "https://randomuser.me/api?results=5";
 
-  const getUsers = async (resultNumber) => {
-    const res = await fetch(url + `?results=${resultNumber}`);
+  const getUsers = async () => {
+    const res = await fetch(url);
     const data = await res.json();
     setUsers(data.results);
-  };
-
-  useEffect(() => {
-    getUsers(numberOfUsers);
-  }, [numberOfUsers]);
-
-  const handleInputChange = (event) => {
-    setNumberOfUsers(event.target.value);
   };
 
   return (
     <div className="app">
       <h1>Random User Generator</h1>
-      <RangeInput
-        id="user-range"
-        label={`Number of users: ${numberOfUsers}`}
-        min={1}
-        max={10}
-        value={numberOfUsers}
-        onChange={handleInputChange}
-      />
+      <Button onClick={getUsers} label="Get Random Users" />
       <CardContainer cards={users} />
     </div>
   );
